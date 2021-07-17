72db31e06aff059d26216dc4eb5cb823b26de037
gleb-t <gleb-t@users.noreply.github.com>
2021-07-17 14:08:10 +0200

    Updated the benchmark to plot the results.

--- benchmark_azul.py
+++ benchmark_azul.py
@@ -1,8 +1,13 @@
 import operator
+import os
 import random
+from pathlib import Path
 from typing import *
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from azulbot.azulsim import Azul, Move, AzulState
 from lib.StageTimer import StageTimer
@@ -66,69 +71,100 @@ class MctsBotWrapper:
 
 
 def main():
-    gamesToPlay = 10
+    gamesToPlay = 30
     maxRoundsPerGame = 100
-    mctsBudget = 100000
+    mctsBudgets = [1000, 10000, 100000, 1000000]
+    # mctsBudgets = [100, 1000]
     samplingWidth = 10
 
-    azul = Azul()
-    # players = [build_random_bot(), build_mcts_bot(mctsBudget)]
-    players = [build_greedy_bot(), MctsBotWrapper(mctsBudget, samplingWidth)]
-    scores = []
-    timePerMove = []
+    outDirPath = Path(os.environ['DEV_OUT_PATH']) / 'azul_bot' if 'DEV_OUT_PATH' in os.environ else Path.cwd()
+    outDirPath.mkdir(parents=True, exist_ok=True)
 
-    timer = StageTimer()
-    for iGame in range(gamesToPlay):
-        timer.start_pass()
+    resultRows = []
 
-        state = azul.get_init_state()
+    for budget in mctsBudgets:
+        azul = Azul()
+        # players = [build_random_bot(), build_mcts_bot(mctsBudget)]
+        players = [build_greedy_bot(), MctsBotWrapper(budget, samplingWidth)]
+        scores = []
+        timePerMove = []
 
-        roundCount = 0
-        moveCount = 0
-        for _ in range(maxRoundsPerGame):
-            timer.start_stage('deal')
-            state = azul.deal_round(state)
+        timer = StageTimer()
+        for iGame in range(gamesToPlay):
+            timer.start_pass()
 
-            while not azul.is_round_end(state):
-                timer.start_stage('decide')
-                move = players[state.nextPlayer](state)
-                timer.start_stage('move')
-                state = azul.apply_move_without_scoring(state, move).state
-                moveCount += 1 if state.nextPlayer == 1 else 0
+            state = azul.get_init_state()
 
-            timer.start_stage('score')
-            state = azul.score_round(state)
-            roundCount += 1
+            roundCount = 0
+            moveCount = 0
+            for _ in range(maxRoundsPerGame):
+                timer.start_stage('deal')
+                state = azul.deal_round(state)
+
+                while not azul.is_round_end(state):
+                    timer.start_stage('decide')
+                    move = players[state.nextPlayer](state)
+                    timer.start_stage('move')
+                    state = azul.apply_move_without_scoring(state, move).state
+                    moveCount += 1 if state.nextPlayer == 1 else 0
+
+                timer.start_stage('score')
+                state = azul.score_round(state)
+                roundCount += 1
+
+                if azul.is_game_end(state):
+                    state = azul.score_game(state)
+                    break
+
+            timer.end_stage()
 
             if azul.is_game_end(state):
-                state = azul.score_game(state)
-                break
+                timer.end_pass()
+                dur = timer.get_pass_duration()
+                scores.append([state.players[0].score, state.players[1].score])
+                timePerMove.append(timer.get_pass_timings()['decide'] / moveCount)
+                print(f"Finished a game with scores {state.players[0].score}:{state.players[1].score}"
+                      f" in {roundCount} rounds and {dur:.2f} s.")
 
-        timer.end_stage()
+            else:
+                print("Timed out playing a game.")
 
-        if azul.is_game_end(state):
-            timer.end_pass()
-            dur = timer.get_pass_duration()
-            scores.append([state.players[0].score, state.players[1].score])
-            timePerMove.append(timer.get_pass_timings()['decide'] / moveCount)
-            print(f"Finished a game with scores {state.players[0].score}:{state.players[1].score}"
-                  f" in {roundCount} rounds and {dur:.2f} s.")
+        timer.end()
 
-        else:
-            print("Timed out playing a game.")
+        scoresArray = np.array(scores)
+        scoresAvg = scoresArray.mean(axis=0)
+        winsFirst = np.count_nonzero(scoresArray[:, 0] > scoresArray[:, 1])
+        winsSecond = np.count_nonzero(scoresArray[:, 0] < scoresArray[:, 1])
+        # Could be a draw.
 
-    timer.end()
+        for i in range(scoresArray.shape[0]):
+            resultRows.append({
+                'player': 'greedy',
+                'budget': 0,
+                'score': scoresArray[i, 0],
+                'isWin': scoresArray[i, 0] > scoresArray[i, 1]
+            })
 
-    scoresArray = np.array(scores)
-    scoresAvg = scoresArray.mean(axis=0)
-    winsFirst = np.count_nonzero(scoresArray[:, 0] > scoresArray[:, 1])
-    winsSecond = np.count_nonzero(scoresArray[:, 0] < scoresArray[:, 1])
-    # Could be a draw.
+            resultRows.append({
+                'player': 'mcts',
+                'budget': budget,
+                'score': scoresArray[i, 1],
+                'isWin': scoresArray[i, 0] < scoresArray[i, 1]
+            })
 
-    print(timer.get_total_report())
-    print("Average scores: {:.1f} {:.1f}".format(*tuple(scoresAvg)))
-    print("Average time per move: {:.1f}".format(np.mean(np.array(timePerMove))))
-    print("Wins: {} vs {}".format(winsFirst, winsSecond))
+        print(timer.get_total_report())
+        print("Average scores: {:.1f} {:.1f}".format(*tuple(scoresAvg)))
+        print("Average time per move: {:.1f}".format(np.mean(np.array(timePerMove))))
+        print("Wins: {} vs {}".format(winsFirst, winsSecond))
+
+    print("Plotting.")
+    resultTable = pd.DataFrame(resultRows)
+    print(resultTable)
+    ax = sns.boxplot(x='budget', y='score', hue='player', data=resultTable)
+    ax.get_figure().savefig(outDirPath / 'scores.pdf')
+
+    plt.show()
+
     print("Done.")
 
 
